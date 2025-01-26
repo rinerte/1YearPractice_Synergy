422b866c8ecb52fcccc43e072da02705bcc4711a
rinerte <rinerte@gmail.com>
2025-01-26 13:48:20 +0900

    unsolved yet

--- 1Task_Go/main.go
+++ 1Task_Go/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Print("Hello")
+	var max = math.MinInt64
+	var min = math.MaxInt64
+
+	var totalSum, minSum, maxSum int
+
+	for i := 1; i < len(os.Args); i++ {
+		var number, error = strconv.Atoi(os.Args[i])
+		if error != nil {
+			panic(error)
+		}
+
+		totalSum += number
+		if number < min {
+			min = number
+			minSum = totalSum
+		}
+		if number > max {
+			max = number
+			maxSum = totalSum
+		}
+	}
+
+	var result = maxSum - minSum
+	if result < 0 {
+		result = -result
+	}
+	fmt.Println(minSum)
+	fmt.Println(maxSum)
+	fmt.Println("The sum between Max and Min elements = ", result)
 }
