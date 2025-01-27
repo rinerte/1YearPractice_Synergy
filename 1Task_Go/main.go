3d1bfc15a1bcc667f5710837b2c1178e0f2c2cee
rinerte <rinerte@gmail.com>
2025-01-27 10:50:35 +0900

    refactored, tests added

--- 1Task_Go/main.go
+++ 1Task_Go/main.go
@@ -8,32 +8,44 @@ import (
 )
 
 func main() {
+	result, err := computeSumBetweenMaxAndMin(os.Args)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
+
+func computeSumBetweenMaxAndMin(args []string) (int, error) {
 	var max = math.MinInt64
 	var min = math.MaxInt64
 
 	var totalSum, minSum, maxSum int
+	var right = true
 
-	for i := 1; i < len(os.Args); i++ {
-		var number, error = strconv.Atoi(os.Args[i])
-		if error != nil {
-			panic(error)
+	for i := 1; i < len(args); i++ {
+		number, err := strconv.Atoi(args[i])
+		if err != nil {
+			return 0, err
 		}
 
-		
 		if number < min {
 			min = number
 			minSum = totalSum
+			right = false
 		}
 		if number > max {
 			max = number
 			maxSum = totalSum
+			right = true
 		}
 		totalSum += number
 	}
-
-	var result = maxSum - minSum
-	if result<0 {
-		result = -result - max
+	var result = 0
+	if right {
+		result = maxSum - minSum - min
+	} else {
+		result = minSum - maxSum - max
 	}
-	fmt.Println(result)
+
+	return result, nil
 }
---
+++ 1Task_Go/main_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestComputeSumBetweenMaxAndMin(t *testing.T) {
+	tests := []struct {
+		args    []string
+		want    int
+		wantErr bool
+	}{
+		{
+			args: []string{"program", "0", "5", "3", "2", "1", "4", "6", "8", "7", "9"},
+			want: 36,
+		},
+		{
+			args: []string{"program", "9", "8", "1", "4", "3", "7", "2", "6", "5", "0"},
+			want: 36,
+		},
+		{
+			args: []string{"program", "1", "2", "0", "7", "6", "5", "9", "8", "4", "3"},
+			want: 18,
+		},
+		{
+			args: []string{"program", "5", "4", "9", "1", "2", "3", "0", "6", "7", "8"},
+			want: 6,
+		},
+		{
+			args: []string{"program", "1", "2", "8", "9", "3", "4", "5", "6", "7", "0"},
+			want: 25,
+		},
+		{
+			args: []string{"program", "9", "1", "2", "3", "4", "0", "8", "7", "6", "5"},
+			want: 10,
+		},
+		{
+			args: []string{"program", "0", "1", "6", "7", "9", "8", "4", "5", "3", "2"},
+			want: 14,
+		},
+		{
+			args: []string{"program", "1", "2", "9"},
+			want: 2,
+		},
+		{
+			args: []string{"program", "-5", "-2", "9"},
+			want: -2,
+		},
+		{
+			args: []string{"program", "-5", "-2", "-1","0","3"},
+			want: -3,
+		},
+		{
+			args:    []string{"program", "a", "b", "c"},
+			wantErr: true, // Invalid input
+		},
+	}
+
+	var count =0
+	for _, tt := range tests {
+		
+		t.Run("", func(t *testing.T) {
+			got, err := computeSumBetweenMaxAndMin(tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("computeSumBetweenMaxAndMin() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("computeSumBetweenMaxAndMin() = %v, want %v", got, tt.want)
+			} else {
+				fmt.Println(" got:", got, " :: OK", "params:",tt.args)
+				count++
+			}			
+		})
+	}
+	fmt.Printf("Total tests = %v, test passed = %v \n",len(tests),count)
+}
